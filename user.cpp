a1313cea3c26b70bcc2683220aad8b0daa400733
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-15 02:01:18 +0300

    Added the implemetation of the getters and setters

--- user.cpp
+++ user.cpp
@@ -13,3 +13,16 @@ User::User(CustomString& name, CustomString& lastName, CustomString& email, Cust
 {
     setEmail(email);
 }
+
+CustomString User::getName() const{return name;}
+CustomString User::getLastName() const{return lastName;}
+uint8_t User::getId() const{return id;}
+CustomString User::getEmail() const{return email;}
+CustomVector<Mail> User::getMails() const{return mails;};
+
+void User::setName(CustomString& name) {this->name = name;}
+void User::setLastName(CustomString& lastName) {this->lastName = lastName;}
+void User::setEmail(CustomString& email) {
+    this->email = email; // i will have to verify if the paremeter has required format
+}
+void User::setMail(CustomVector<Mail>& mails) {this->mails = mails;}
