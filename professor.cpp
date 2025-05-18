72f5309ace0ab15d4a39cd99a5e72a6ba107cf4a
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-19 01:49:38 +0300

    Class name changed. SendMailCourse implemented

--- professor.cpp
+++ professor.cpp
@@ -2,25 +2,36 @@
 
 #include "professor.h"
 
-Professor::Professor() {}
-Professor::Professor(CustomString& name, CustomString& lastName, CustomString& email, CustomVector<Mail>& mails) :
+Teacher::Teacher() {}
+Teacher::Teacher(CustomString& name, CustomString& lastName, CustomString& email, CustomVector<Mail>& mails) :
     User(name, lastName, email, mails) {}
 
-void Professor::createCourse(CustomString& name, CustomString& password, CustomVector<Assignment>& assignments, CustomVector<Student>& studentsMembers) {
+void Teacher::createCourse(CustomString& name, CustomString& password, CustomVector<Assignment>& assignments, CustomVector<Student>& studentsMembers) {
     Course course(name, password, assignments, studentsMembers);
     this->courses.push_back(course);
 }
 
-void Professor::createAssignment(Course& course, CustomString& name) {
+void Teacher::createAssignment(Course& course, CustomString& name) {
     Assignment assignment(name);
     course.getAssignments().push_back(assignment);
 }
 
-void Professor::enrollStudent(Course& course, Student& student) {course.getStudentsMembers().push_back(student);}
+void Teacher::enrollStudent(Course& course, Student& student) {course.getStudentsMembers().push_back(student);}
 
-void Professor::gradeAssignment(Assignment& assignment, uint8_t idStudent, uint8_t grade) {
+void Teacher::gradeAssignment(Assignment& assignment, uint8_t idStudent, uint8_t grade) {
     int i = 0;
     while (assignment.getAnswers()[i].getIdStudent() == idStudent) {
         assignment.getAnswers()[i].setGrade(grade);
     }
+}
+
+void Teacher::sendMailsCourse(& mail, CustomString courseName) {
+    int i = 0;
+    while (this->courses[i].getName() == courseName) {
+        for (uint8_t i = 0; i < this->courses[i].getStudentsMembers().getSize(); i++) {
+            sendMail()
+        }
+        
+    }
+    
 }
