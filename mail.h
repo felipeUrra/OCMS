36b3ef1fe3643deccde0bc4e88e6d088ed0dc8e8
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-19 01:45:57 +0300

    Minor changes

--- mail.h
+++ mail.h
@@ -5,7 +5,7 @@
 
 class Mail {
 private:
-    static uint8_t nextIdMail();
+    static uint8_t nextIdMail;
 
     User* sender;
     uint8_t idMail;
@@ -13,22 +13,20 @@ private:
     CustomString date; // it will be a class or an struct
     uint8_t time; // it will be a class or an struct;
 
-    static void incrementNextIdMail(uint8_t);
-
 public:
     Mail();
-    Mail(User&, CustomString, CustomString, uint8_t);
+    Mail(User*, CustomString, CustomString, uint8_t);
     ~Mail() = default;
 
     // getters and setters
-    User& getSender() const;
+    User* getSender() const;
     uint8_t getIdMail() const;
     CustomString getContent() const;
-    CustomString  getDate() const;
+    CustomString getDate() const;
     uint8_t getTime() const;
 
     void setSender(User&);
-    void setContent(CustomString);
-    void setDate(CustomString);
+    void setContent(CustomString&);
+    void setDate(CustomString&);
     void setTime(uint8_t);
 };
