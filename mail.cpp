70818ebd3292e635a7fb941e38caa2f20a9d0c3d
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-20 02:25:34 +0300

    Little modifications in the constructors. Commented the setters of date and time. Implemented getActualDateAndTime() and print()

--- mail.cpp
+++ mail.cpp
@@ -1,23 +1,47 @@
 #include "mail.h"
+#include "iostream"
 
-uint8_t Mail::nextIdMail = 0;
+//uint8_t Mail::nextIdMail = 0;
 
-Mail::Mail() : sender(nullptr), idMail(nextIdMail++) {}
-Mail::Mail(User* sender, CustomString content, CustomString date, uint8_t time) :
+Mail::Mail() : sender(nullptr)/*, idMail(nextIdMail++)*/ {
+    getActualDateAndTime();
+}
+
+Mail::Mail(User* sender, CustomString content) :
     sender(sender),
-    idMail(nextIdMail++),
-    content(content),
-    date(date),
-    time(time) {}
+    //idMail(nextIdMail++),
+    content(content)
+{
+    getActualDateAndTime();
+}
 
 // getters and setters
 User* Mail::getSender() const{return this->sender;}
 uint8_t Mail::getIdMail() const{return this->idMail;}
 CustomString Mail::getContent() const{return this->content;}
 CustomString Mail::getDate() const{return this->date;}
-uint8_t Mail::getTime() const{return this->time;}
+CustomString Mail::getTime() const{return this->time;}
 
 void Mail::setSender(User& sender) {this->sender = &sender;}
 void Mail::setContent(CustomString& content) {this->content = content;}
-void Mail::setDate(CustomString& date) {this->date = date;}
-void Mail::setTime(uint8_t time) {this->time = time;}
+//void Mail::setDate(CustomString& date) {this->date = date;}
+//void Mail::setTime(CustomString& time) {this->time = time;}
+
+void Mail::getActualDateAndTime() {
+    time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+
+    // date format: DD-MM-YYYY
+    char buffDate[11]; // DD-MM-YYYY\0
+    std::strftime(buffDate, sizeof(buffDate), "%d.%m.%Y", now);
+    date = buffDate;
+
+    // time format: HH-MM
+    char buffTime[6]; // HH-MM\0
+    std::strftime(buffTime, sizeof(buffTime), "%H:%M", now);
+    time = buffTime;
+}
+
+void Mail::print() {
+    std::cout << time << " " << date << ", sent by " << sender->getName() << " " << sender->getLastName() << ": " << content;
+}
