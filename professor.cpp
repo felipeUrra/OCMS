7ab6b2c791bd39393db60a8afa1352f151786da8
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-21 20:34:08 +0300

    Added implementations of the getter, setters and getSpecificCourse()

--- professor.cpp
+++ professor.cpp
@@ -6,6 +6,10 @@ Teacher::Teacher() {}
 Teacher::Teacher(CustomString& name, CustomString& lastName, CustomString& password) :
     User(name, lastName, password, UserType::Teacher) {}
 
+// getters and setters
+CustomVector<Course>& Teacher::getCourses() {return courses;}
+void Teacher::setCourses(CustomVector<Course>& courses) {this->courses = courses;}
+
 void Teacher::createCourse(CustomString& name, CustomString& password) {
     Course course(name, password);
     this->courses.push_back(course);
@@ -17,6 +21,14 @@ void Teacher::createAssignment(Course& course, CustomString& name) {
 }
 
 void Teacher::enrollStudent(Course& course, User* student) {course.getStudentsMembers().push_back(student);}
+
+Course& Teacher::getSpecificCourse(CustomString& courseName){
+    for (uint8_t i = 0; i < this->courses.getSize(); i++) {
+        if (this->courses[i].getName() == courseName) {
+            return this->courses[i];
+        }
+    }  
+}
 
 void Teacher::gradeAssignment(Assignment& assignment, uint8_t idStudent, uint8_t grade) {
     int i = 0;
