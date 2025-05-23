9dbe8f3fbe517db4d8e23c17048f4b5a29ab7cfd
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-23 14:59:36 +0300

    Implemented all the Student's functions

---
+++ student.cpp
@@ -0,0 +1,21 @@
+#include "student.h"
+
+Student::Student(CustomString& name, CustomString& lastName, CustomString& password) :
+    User(name, lastName, password, UserType::Student) {}
+
+// Getters and setters
+CustomVector<Course*>& Student::getCoursesEnrolled() {return coursesEnrolled;}
+void Student::setCoursesEnrolled(CustomVector<Course*>& coursesEnrolled) {this->coursesEnrolled = coursesEnrolled;}
+
+void Student::printAnswers() {
+    for (uint8_t i = 0; i < this->coursesEnrolled.getSize(); i++) {
+        for (uint8_t j = 0; j < this->coursesEnrolled[i]->getAssignments().getSize(); j++) {
+            for (uint8_t k = 0; k < this->coursesEnrolled[i]->getAssignments()[j]->getAnswers().getSize(); k++) {
+                Answer* a = this->coursesEnrolled[i]->getAssignments()[j]->getAnswers()[k];
+                if (a->getStudentId() == this->getId() && a->getIsGraded() == true) {
+                    std::cout << this->coursesEnrolled[i]->getName() << " | " << this->coursesEnrolled[i]->getAssignments()[j] << " | " << a->getGrade() << " | " << a->getTeacherComment() << "\n";
+                }              
+            }
+        }
+    }
+}
