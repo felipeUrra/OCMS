735afb31feeb18b71a895842e5b9912c95a5fd89
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-23 12:10:45 +0300

    Implemented all the functions of Course

---
+++ course.cpp
@@ -0,0 +1,28 @@
+#include "course.h"
+
+Course::Course(const CustomString& name, const CustomString& password) : name(name), password(password) {}
+
+
+// Getters and setters
+CustomString Course::getName() const{return this->name;}
+CustomString Course::getPassword() const{return this->password;}
+CustomVector<Assignment*>& Course::getAssignments() {return this->assignments;}
+CustomVector<User*>& Course::getStudentsMembers() {return this->studentsMembers;}
+
+void Course::setName(CustomString& name) {this->name = name;}
+void Course::setPassword(CustomString& password) {this->password = password;}
+void Course::setAssignments(CustomVector<Assignment*>& assigments) {this->assignments = assigments;}
+void Course::setStudentsMembers(CustomVector<User*>& studentsMembers) {
+    
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < studentsMembers.getSize(); i++) {
+        if (studentsMembers[i]->getUserType() == UserType::Student) {count++;}
+    }
+    
+    if (count == studentsMembers.getSize()) {
+        this->studentsMembers = studentsMembers;
+        return;
+    }
+    
+    // maybe write something or handle it like a exception
+}
