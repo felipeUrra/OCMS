d26df1f15cdedd156c6c7db0de0171305aee24ca
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-05-24 01:07:43 +0300

    Implemented all the class's functions

---
+++ answer.cpp
@@ -0,0 +1,17 @@
+// Felipe Urra Rivadeneira 0MI8000066
+#include "answer.h"
+
+Answer::Answer(Student*  owner, CustomString& answerText) : student(owner), answerText(answerText), isGraded(false) {}
+
+// Getters and setters
+Student* Answer::getStudent() {return this->student;}
+CustomString Answer::getAnswerText() const{return this->answerText;}
+CustomString Answer::getTeacherComment() const{return this->teacherComment;}
+double Answer::getGrade() const{return this->grade;}
+bool Answer::getIsGraded() const{return this->isGraded;}
+
+void Answer::setStudent(Student* student) {this->student = student;}
+void Answer::setAnswerText(CustomString& answerText) {this->answerText = answerText;}
+void Answer::setTeacherCommet(CustomString& teacherComment) {this->teacherComment = teacherComment;}
+void Answer::setGrade(double grade) {this->grade = grade;}
+void Answer::setIsGraded(bool isGraded) {this->isGraded = isGraded;}
